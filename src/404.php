12e1d3097d279c53b6e70f850886eb58c73d1693
axiomattik <axiomattik@pocketgecko.co.uk>
2021-04-14 06:45:09 +0100

    simplify 404 page

--- src/404.php
+++ src/404.php
@@ -14,42 +14,21 @@ get_header();
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'vanilla-breeze' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( 'Sorry! We can\'t find that page.', 'vanilla-breeze' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'vanilla-breeze' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
+				<p>
+					<a href="<?php echo get_home_url(); ?>"><?php esc_html_e( 'Return home', 'vanilla-breeze' ); ?></a>, 
+					<?php 
+						/* translators: this string follows 'Return home'. */
+						esc_html_e( 'try one of the links below or search.', 'vanilla-breeze' );
 					?>
+				</p>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'vanilla-breeze' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
+				<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-					<?php
-					/* translators: %1$s: smiley */
-					$vnbt_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'vanilla-breeze' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$vnbt_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+				<?php get_search_form(); ?>
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
