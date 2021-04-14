8ca02c997a67b2b56336cebd02bee81e0017cad1
axiomattik <axiomattik@pocketgecko.co.uk>
2021-04-14 06:49:36 +0100

    show excerpts on home page

--- src/template-parts/content.php
+++ src/template-parts/content.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * Template part for displaying posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * Template part for post excerpts.
  *
  * @package Vanilla_Breeze
  */
@@ -12,11 +10,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+		the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 
 		if ( 'post' === get_post_type() ) :
 			?>
@@ -24,6 +18,7 @@
 				<?php
 				vnbt_posted_on();
 				vnbt_posted_by();
+				vnbt_category_tags()
 				?>
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
@@ -31,23 +26,13 @@
 
 	<?php vnbt_post_thumbnail(); ?>
 
-	<div class="entry-content">
+	<div class="entry-excerpt">
 		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'vanilla-breeze' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
+		if ( is_singular() ) {
+			the_content();
+		} else {
+			the_excerpt();
+		}
 		wp_link_pages(
 			array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'vanilla-breeze' ),
@@ -55,7 +40,7 @@
 			)
 		);
 		?>
-	</div><!-- .entry-content -->
+	</div><!-- .entry-excerpt-->
 
 	<footer class="entry-footer">
 		<?php vnbt_entry_footer(); ?>
